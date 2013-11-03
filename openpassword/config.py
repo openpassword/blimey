84bfab8b85508a5a4149be76fe4056f10df480e5
Marco Lopes <marco.paulo.lopes@gmail.com>
2013-11-03 20:25:19 +0000

    Give more meaningful name to keychain path config

--- openpassword/config.py
+++ openpassword/config.py
@@ -2,10 +2,10 @@ import traceback
 
 class Config:
     def __init__(self):
-        self._path = None
+        self._keychain_path = None
 
     def set_path(self, path = None):
-        self._path = path
+        self._keychain_path = path
 
     def get_path(self):
-        return self._path
+        return self._keychain_path
