e409a88f0b5f44e44e9438963078ab1cea915539
Marco Lopes <marco.paulo.lopes@gmail.com>
2013-11-09 00:03:59 +0000

    Remove tissue and add script to run pep8 after nose

--- openpassword/exceptions/__init__.py
+++ openpassword/exceptions/__init__.py
@@ -1,1 +1,1 @@
-from openpassword.exceptions.invalid_password_exception import InvalidPasswordException  # nopep8
+from openpassword.exceptions.invalid_password_exception import InvalidPasswordException
