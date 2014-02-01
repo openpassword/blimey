9b9d7dc303dd9f25571c91109ab9d27b4bfe3515
Niko Nevala <niko@nevala.fi>
2014-02-01 21:36:03 +0000

    Make encryption key repository work similarly to item repository

--- openpassword/encryption_key_repository.py
+++ openpassword/encryption_key_repository.py
@@ -1,10 +1,30 @@
+import json
+
+
 class EncryptionKeyRepository:
-    def __init__(self, keys):
-        self.keys = keys
+    def __init__(self, path):
+        self._path = path
 
     def key_for_security_level(self, security_level):
-        identifier = self.keys[security_level]
+        key_file_path = self._resolve_key_file_path()
+        keys = self._load_key_data(key_file_path)
 
-        for key in self.keys["list"]:
+        identifier = keys[security_level]
+
+        for key in keys["list"]:
             if key["identifier"] == identifier:
                 return key
+
+    def _resolve_key_file_path(self):
+        return self._path + '/data/default/encryptionKeys.js'
+
+    def _load_key_data(self, path):
+        try:
+            file = open(path)
+        except IOError:
+            raise InvalidUuidException("Invalid path: %s" % path)
+
+        data = json.load(file)
+        file.close()
+
+        return data
