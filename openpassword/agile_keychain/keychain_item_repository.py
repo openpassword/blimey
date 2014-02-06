7fa9a97711cbb1907d70b7795535577775c47ecb
Marco Lopes <marco.paulo.lopes@gmail.com>
2014-02-06 11:41:48 +0000

    A bit of refactoring on the item repository

--- openpassword/agile_keychain/keychain_item_repository.py
+++ openpassword/agile_keychain/keychain_item_repository.py
@@ -11,14 +11,14 @@ class KeychainItemRepository:
 
     def get_item_by_unique_id(self, unique_id):
         keychain_item_path = self._resolve_keychain_item_path(unique_id)
-        keychain_item = self._load_keychain_item_data(keychain_item_path)
+        keychain_item_data = self._load_keychain_item_data(keychain_item_path)
 
-        return KeychainItem(keychain_item)
+        return KeychainItem(keychain_item_data)
 
     def filter(self, callback):
         items = []
 
-        for path in glob(self._path + '/data/default/*.1password'):
+        for path in glob(self._resolve_keychain_item_path('*')):
             keychain_item = self._load_keychain_item_data(path)
             item = KeychainItem(keychain_item)
             if callback(item):
@@ -27,13 +27,13 @@ class KeychainItemRepository:
         return items
 
     def _resolve_keychain_item_path(self, uuid):
-        return self._path + '/data/default/%s.1password' % uuid
+        return self._path + '/data/default/{0}.1password'.format(uuid)
 
     def _load_keychain_item_data(self, path):
         try:
             file = open(path)
         except IOError:
-            raise InvalidUuidException("Invalid path: %s" % path)
+            raise InvalidUuidException("Invalid path: {0}".format(path))
 
         data = json.load(file)
         file.close()
