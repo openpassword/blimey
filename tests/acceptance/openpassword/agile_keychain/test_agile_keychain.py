086d1f802b8478dd847364e9c77d792f8b388145
Marco Lopes <marco.paulo.lopes@gmail.com>
2014-02-12 12:54:16 +0000

    #48 Add some acceptance tests for the keychain

--- tests/acceptance/openpassword/agile_keychain/test_agile_keychain.py
+++ tests/acceptance/openpassword/agile_keychain/test_agile_keychain.py
@@ -1,6 +1,14 @@
 from nose.tools import *
+from nose import SkipTest
 import os
 import openpassword
+
+
+def skip(f):
+    """ Decorator to indicate a test should be skipped."""
+    def g(*args, **kw):
+        raise SkipTest()
+    return make_decorator(f)(g)
 
 
 class AgileKeychainTest:
@@ -10,9 +18,27 @@ class AgileKeychainTest:
 
         self.keychain = openpassword.AgileKeychain(fixture_path)
 
-    def test_unlock_and_get_item_by_unique_id(self):
+    def test_unlock_keychain(self):
+        eq_(self.keychain.is_locked(), True)
+        self.keychain.unlock("masterpassword123")
+
+        eq_(self.keychain.is_locked(), False)
+
+    def test_get_item_by_unique_id(self):
         self.keychain.unlock("masterpassword123")
         item = self.keychain.get_item_by_unique_id("97019BEBCF9E402F8F0C033474B1B85D")
 
         eq_(item.data["fields"][0]["value"], "somedifferentusername")
         eq_(item.data["fields"][1]["value"], "password123")
+
+    @skip
+    def test_get_all_items(self):
+        pass
+
+    def test_lock_keychain(self):
+        eq_(self.keychain.is_locked(), True)
+        self.keychain.unlock("masterpassword123")
+
+        eq_(self.keychain.is_locked(), False)
+        self.keychain.lock()
+        eq_(self.keychain.is_locked(), True)
