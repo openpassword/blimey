8ecd0548fd07a73c0f7d95af6693476acebf5916
Marco Lopes <marco.paulo.lopes@gmail.com>
2014-02-18 17:24:27 +0000

    #48 Replace fudge acceptance tests with behave tests

--- features/keychain.feature
+++ features/keychain.feature
@@ -9,3 +9,14 @@ Scenario: Getting item by unique id
   Given I have an unlocked keychain
   When I request item with id "9E7673CCBB5B4AC9A7A8838835CB7E83"
   Then I should get an item with the same id
+
+  Scenario: Getting all items of a keychain
+    Given I have an unlocked keychain
+    And the keychain has 9 items
+    When I request all items from the keychain
+    Then I should get a collection with 9 items
+
+  Scenario: Locking a keychain
+    Given I have an unlocked keychain
+    When I lock the keychain
+    Then it will become locked
--- features/steps/keychain_steps.py
+++ features/steps/keychain_steps.py
@@ -1,11 +1,13 @@
 from behave import given, when, then, step
 import os
 import openpassword
+from openpassword.item_collection import ItemCollection
+from glob import glob
 
 
 @given('I have a locked keychain')
 def step_impl(context):
-    context.keychain = _get_fixture_path()
+    context.keychain = _get_keychain()
     assert context.keychain.is_locked() is True
 
 
@@ -21,7 +23,7 @@ def step_impl(context):
 
 @given('I have an unlocked keychain')
 def step_impl(context):
-    context.keychain = _get_fixture_path()
+    context.keychain = _get_keychain()
     context.keychain.unlock("masterpassword123")
     assert context.keychain.is_locked() is False
 
@@ -37,7 +39,38 @@ def step_impl(context):
     assert context.item.uuid == context.item_unique_id
 
 
-def _get_fixture_path():
+@given('the keychain has 9 items')
+def step_impl(context):
+    assert len(glob(_get_keychain_path() + "/data/default/*.1password")) is 9
+
+
+@when('I request all items from the keychain')
+def step_impl(context):
+    context.items = context.keychain.all_items()
+
+
+@then('I should get a collection with 9 items')
+def step_impl(context):
+    assert type(context.items) is ItemCollection
+    assert len(context.items) is 9
+
+
+@when('I lock the keychain')
+def step_impl(context):
+    context.keychain.lock()
+
+
+@then('it will become locked')
+def step_impl(context):
+    assert context.keychain.is_locked() is True
+
+
+def _get_keychain():
     current_path = os.path.dirname(os.path.realpath(__file__))
     fixture_path = current_path + '/../../tests/fixtures/test.agilekeychain'
     return openpassword.AgileKeychain(fixture_path)
+
+
+def _get_keychain_path():
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    return current_path + '/../../tests/fixtures/test.agilekeychain'
