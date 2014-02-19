4d9fc40565328f356839f1d07c09acf6ac165880
Marco Lopes <marco.paulo.lopes@gmail.com>
2014-02-19 11:55:34 +0000

    #48 refactor acceptance tests

--- features/steps/keychain_steps.py
+++ features/steps/keychain_steps.py
@@ -4,6 +4,8 @@ import openpassword
 from openpassword.item_collection import ItemCollection
 from glob import glob
 
+MASTER_PASSWORD = "masterpassword123"
+
 
 @given('I have a locked keychain')
 def step_impl(context):
@@ -13,7 +15,7 @@ def step_impl(context):
 
 @when('I unlock it')
 def step_impl(context):
-    context.keychain.unlock("masterpassword123")
+    context.keychain.unlock(MASTER_PASSWORD)
 
 
 @then('it will become unlocked')
@@ -24,15 +26,14 @@ def step_impl(context):
 @given('I have an unlocked keychain')
 def step_impl(context):
     context.keychain = _get_keychain()
-    context.keychain.unlock("masterpassword123")
+    context.keychain.unlock(MASTER_PASSWORD)
     assert context.keychain.is_locked() is False
 
 
 @given('the keychain has an item with id "{item_id}"')
 def step_impl(context, item_id):
     context.item_id = item_id
-    assert len([a for a in glob(_get_keychain_path() + "/data/default/*.1password") if
-                a.endswith(context.item_id + ".1password") > 0])
+    assert len(glob("{0}/data/default/{1}.1password".format(_get_keychain_path(), context.item_id))) > 0
 
 
 @when('I request item with the given id')
@@ -47,7 +48,7 @@ def step_impl(context):
 
 @given('the keychain has a given number of items')
 def step_impl(context):
-    context.keychain_len = len(glob(_get_keychain_path() + "/data/default/*.1password"))
+    context.keychain_len = len(glob("{0}/data/default/*.1password".format(_get_keychain_path())))
 
 
 @when('I request all items from the keychain')
@@ -72,11 +73,9 @@ def step_impl(context):
 
 
 def _get_keychain():
-    current_path = os.path.dirname(os.path.realpath(__file__))
-    fixture_path = current_path + '/../../tests/fixtures/test.agilekeychain'
-    return openpassword.AgileKeychain(fixture_path)
+    return openpassword.AgileKeychain(_get_keychain_path())
 
 
 def _get_keychain_path():
     current_path = os.path.dirname(os.path.realpath(__file__))
-    return current_path + '/../../tests/fixtures/test.agilekeychain'
+    return '{0}/../../tests/fixtures/test.agilekeychain'.format(current_path)
