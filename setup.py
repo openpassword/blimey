584cddb14a065b2b1917a84454ee510eb9e46b50
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2015-04-04 16:49:09 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='blimey',
+      version='0.1',
+      description='Library for reading AgileKeychain files',
+      author='openpassword',
+      url='https://github.com/openpassword/blimey',
+      packages=[
+          'blimey',
+          'blimey.abstract',
+          'blimey.agile_keychain',
+          'blimey.agile_keychain._manager',
+          'blimey.agile_keychain.template',
+      ])
